e8618032e1595ab0dbd8af0f76ea044102e2eb61
meduna femi <femimeduna@gmail.com>
2022-02-22 11:47:59 -0700

    load planets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,19 @@
 const request = require('supertest')
 const app = require('../../app')
-const { mongoConnect, mongoDisconnect } = require('../../services/mongo')
+const { 
+    mongoConnect, 
+    mongoDisconnect 
+} = require('../../services/mongo')
 
+const { 
+    loadPlanetsData 
+} = require('../../models/planets.model')
 
 
 describe('Launches API', () => {
     beforeAll(async () => {
         await mongoConnect()
+        await loadPlanetsData()
     })
 
     afterAll(async () => {
